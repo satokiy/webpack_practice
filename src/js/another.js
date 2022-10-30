3d66e503830d4c4ff1322604ee80bbc3567780c7
satokiy <syamada@netprotections.co.jp>
2022-10-31 00:21:41 +0900

    prettier

--- src/js/another.js
+++ src/js/another.js
@@ -1,9 +1,9 @@
 import $ from 'jquery';
 import velocity from 'velocity-animate';
-import { greet } from './modules/greet'
+import { greet } from './modules/greet';
 
 $('body')
-.css('color', 'red')
-.append(`<p>${greet('App')}</p>`);
+  .css('color', 'red')
+  .append(`<p>${greet('App')}</p>`);
 
 velocity($('h1'), 'fadeIn', { duration: 2000, loop: true });
