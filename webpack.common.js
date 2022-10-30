2264bd0fa014d47cf9f69c5710458d2e8a48a400
satokiy <syamada@netprotections.co.jp>
2022-10-30 22:34:26 +0900

    split-chunks-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,25 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'js/[name].bundle.js',
+    chunkFilename: 'js/[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'initial',
+      cacheGroups: {
+        vendor: { // 何でもOK
+          test: /node_modules/, // 分割の対象
+          name: 'vendor', // 出力ファイル名
+        },
+        vendorModules: {
+          test: /src[\\/]js[\\/]modules/, // 分割の対象
+          name: 'vendor-modules', // 出力ファイル名
+          minSize: 0,
+          minChunks: 2, // モジュールがいくつの場所で使われていれば分割の対象になるか
+        },
+      }
+      
+    }
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -22,6 +41,5 @@ module.exports = {
       template: './src/html/another.html', // 入力ファイル名
       chunks: ['another'], // バンドルしたjsのどれを読み込むか。指定がなければすべて読み込まれる
     }),
-    
   ]
 };
